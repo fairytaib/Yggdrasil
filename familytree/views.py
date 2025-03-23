ac1be9cea05f29c9831cac80d68e6623224fdfe0
fairytaib <fairytaib@gmail.com>
2025-03-23 15:34:41 +0100

    Create Basic View
    Lets me check if the Data can be displayed

--- familytree/views.py
+++ familytree/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404
+from .models import Person, FamilyTree
+from django.contrib.auth.models import User
 # Create your views here.
+
+
+def family_list(request):
+    families = Person.objects.all()
+    return render(request, 'familytree/family-list.html',
+                  {'families': families})
