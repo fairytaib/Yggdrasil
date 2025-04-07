7b3e5f3c9745dc1c1e72d70cff8a90e01d2320de
fairytaib <fairytaib@gmail.com>
2025-04-07 10:31:12 +0200

    Set up connection to cloudinary

--- familytree/models.py
+++ familytree/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
 
 # Create your models here.
@@ -27,6 +28,7 @@ class Person(models.Model):
         User, on_delete=models.CASCADE,
         related_name='persons'
     )
+    featured_image = CloudinaryField('image', default='placeholder')
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
     birth_date = models.DateField(null=True, blank=True)
