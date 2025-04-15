ba04ceb0ff735586097ec923f5714bbe2b5ffa72
fairytaib <fairytaib@gmail.com>
2025-04-15 10:54:26 +0200

    Write Homepage/Startview

--- static/javascript/family_view.js
+++ static/javascript/family_view.js
@@ -10,6 +10,7 @@
 
 function toggleSection(id) {
     // Toggle the display of the section with the given id
+    // and change the button text accordingly
     const section = document.getElementById(id);
     if (section.style.display === "none") {
         section.style.display = "block";
