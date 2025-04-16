1d90846ab5594467f9d87750b011a46176991f56
fairytaib <fairytaib@gmail.com>
2025-04-16 10:34:21 +0200

    Add design

--- staticfiles/javascript/family_view.js
+++ staticfiles/javascript/family_view.js
@@ -1,5 +1,16 @@
+// function toggleButtonText(id){
+//     // Toggle the text of the button with the given id
+//     const button = document.getElementById(id);
+//     if (button.innerHTML === "Show") {
+//         button.innerHTML = "Hide";
+//     } else {
+//         button.innerHTML = "Show";
+//     }
+// }
+
 function toggleSection(id) {
     // Toggle the display of the section with the given id
+    // and change the button text accordingly
     const section = document.getElementById(id);
     if (section.style.display === "none") {
         section.style.display = "block";
