5e874e33d05e846303bf88037c8ec8b0b295cf3d
fairytaib <fairytaib@gmail.com>
2025-04-17 20:01:03 +0200

    Make Debug depending on enviroment

--- main/settings.py
+++ main/settings.py
@@ -28,7 +28,9 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+
+ENVIRONMENT = os.environ.get("ENV", "dev") # dev means development
+DEBUG = ENVIRONMENT == "dev"
 
 ALLOWED_HOSTS = [".herokuapp.com", "127.0.0.1"]
 
