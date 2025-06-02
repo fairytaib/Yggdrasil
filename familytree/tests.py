6ff8494fd3a24c7203b7cacae03d4a7f3777d785
fairytaib <fairytaib@gmail.com>
2025-06-02 19:56:28 +0200

    Write unit test fot Add self

--- familytree/tests.py
+++ familytree/tests.py
@@ -1,10 +1,11 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 from familytree.forms import PersonForm
 from familytree.forms import FamilyRelationForm
 from familytree.models import FamilyRelation
 from django.contrib.auth.models import User
 from django.core.files.uploadedfile import SimpleUploadedFile
-from familytree.models import Person
+from familytree.models import Person, FamilyTree
+from django.urls import reverse
 from datetime import date, timedelta
 import io
 
@@ -229,3 +230,64 @@ class FamilyRelationFormTest(TestCase):
         )
         self.assertFalse(form.is_valid())
         self.assertIn('relation_type', form.errors)
+
+
+class AddSelfViewTest(TestCase):
+    """Tests for the add_self view."""
+
+    def setUp(self):
+        self.client = Client()
+        self.user = User.objects.create_user(username="tester",
+                                             password="testpass")
+        self.url = reverse("add_self")
+
+    def test_redirect_if_not_logged_in(self):
+        """Unauthenticated users should be redirected to login."""
+        response = self.client.get(self.url)
+        self.assertNotEqual(response.status_code, 200)
+        self.assertRedirects(response, f"/accounts/login/?next={self.url}")
+
+    def test_get_returns_form(self):
+        """GET request should return a form."""
+        self.client.login(username="tester", password="testpass")
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "<form")
+        self.assertTemplateUsed(response, "familytree/add_self.html")
+
+    def test_post_creates_person_and_family_tree(self):
+        """Valid POST should create Person and FamilyTree and redirect."""
+        self.client.login(username="tester", password="testpass")
+        data = {
+            "first_name": "Fatima",
+            "last_name": "al-Fihri",
+            "birth_place": "Fez",
+            "birth_country": "MA",
+        }
+        response = self.client.post(self.url, data, follow=True)
+
+        self.assertEqual(Person.objects.count(), 1)
+        person = Person.objects.first()
+        self.assertEqual(person.first_name, "Fatima")
+        self.assertEqual(person.owner, self.user)
+
+        self.assertEqual(FamilyTree.objects.count(), 1)
+        tree = FamilyTree.objects.get(owner=self.user)
+        self.assertIn(person, tree.person.all())
+        self.assertEqual(tree.main_person, person)
+
+        self.assertRedirects(response, reverse("family_view",
+                                               args=[person.id]))
+
+    def test_post_with_invalid_data_renders_form_again(self):
+        """If form is invalid, the template
+        should be re-rendered with errors."""
+        self.client.login(username="tester", password="testpass")
+        data = {
+            "first_name": "",  # Required field missing
+            "last_name": "al-Fihri"
+        }
+        response = self.client.post(self.url, data)
+        self.assertEqual(response.status_code, 200)
+        self.assertFormError(response, "form", "first_name",
+                             "This field is required.")
