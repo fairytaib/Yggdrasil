1bf38a8fc7d93f0e54873262a89b70f61746952b
fairytaib <fairytaib@gmail.com>
2025-06-11 15:08:56 +0200

    Test push for better performance rating

--- peopleOfHistory/views.py
+++ peopleOfHistory/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from .models import PersonOfHistory
 from datetime import date
 import hashlib
+from cloudinary.utils import cloudinary_url
 
 
 def person_of_the_day_view(request):
@@ -11,24 +12,30 @@ def person_of_the_day_view(request):
         return render(
             request,
             'peopleOfHistory/people_of_history.html',
-            {'person_of_history': None})
+            {'person_of_history': None}
+        )
 
-    today_str = date.today().isoformat()  # z. B. "2025-04-15"
+    today_str = date.today().isoformat()
     hash_digest = hashlib.md5(today_str.encode()).hexdigest()
     hash_int = int(hash_digest, 16)
 
     index = hash_int % len(all_persons)
     person_of_the_day = all_persons[index]
-
-    image_url = person_of_the_day.image.url
-    if image_url.startswith("http://"):
-        image_url = image_url.replace("http://", "https://")
+    secure_url, _ = cloudinary_url(
+        person_of_the_day.image.public_id,
+        secure=True,
+        transformation=[
+            {"width": 300, "crop": "scale"},
+            {"quality": "auto"},
+            {"fetch_format": "auto"}
+        ]
+    )
 
     return render(
         request,
         'peopleOfHistory/people_of_history.html',
         {
             'person_of_history': person_of_the_day,
-            'secure_image_url': image_url
+            'secure_image_url': secure_url
         }
     )
