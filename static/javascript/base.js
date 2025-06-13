2b5062a6d122aab23171ea788f14bc59043a38a0
fairytaib <fairytaib@gmail.com>
2025-06-13 10:49:49 +0200

    Change duration for messages

--- static/javascript/base.js
+++ static/javascript/base.js
@@ -1,8 +1,8 @@
-//Close Bootstrap Message after 2 seconds 
+//Close Bootstrap Message after a few seconds 
 setTimeout(function () {
     let alertNode = document.querySelector('.alert');
     if (alertNode) {
       let bsAlert = bootstrap.Alert.getOrCreateInstance(alertNode);
       bsAlert.close();
     }
-  }, 2000);
+  }, 4000);
