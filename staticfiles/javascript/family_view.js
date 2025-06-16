c7539b6b8668220f97e06b3787f59f229e2bac64
fairytaib <fairytaib@gmail.com>
2025-06-16 10:54:56 +0200

    Collect static

--- staticfiles/javascript/family_view.js
+++ staticfiles/javascript/family_view.js
@@ -1,20 +1,23 @@
 document.querySelectorAll('.dropdown-item').forEach(item => {
-  item.addEventListener('click', function () {
-    const selected = this.getAttribute('data-value');
+    item.addEventListener('click', function () {
+        const selected = this.getAttribute('data-value');
 
-    // Alle Relationen ausblenden
-    document.querySelectorAll('[data-relation]').forEach(div => {
-      div.style.display = 'none';
+        
+        document.querySelectorAll('[data-relation]').forEach(div => {
+            div.style.display = 'none';
+        });
+
+        
+        const target = document.querySelector(`[data-relation="${selected}"]`);
+        if (target) {
+            target.style.display = 'block';
+        }
+
+        
+        const toggleBtn = document.querySelector('.dropdown-toggle');
+        toggleBtn.textContent = `Relation: ${selected.charAt(0).toUpperCase() + selected.slice(1)}`;
+
+        document.getElementById("description").style.display = "none";
     });
-
-    // Ziel-Relation anzeigen
-    const target = document.querySelector(`[data-relation="${selected}"]`);
-    if (target) {
-      target.style.display = 'block';
-    }
-
-    // Optional: Button-Label ändern
-    const toggleBtn = document.querySelector('.dropdown-toggle');
-    toggleBtn.textContent = `Relation: ${selected.charAt(0).toUpperCase() + selected.slice(1)}`;
-  });
 });
+
